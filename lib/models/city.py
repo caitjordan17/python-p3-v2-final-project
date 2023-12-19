437aa20cb572f80ce17f2a56451f0978d2dd6d52
Caitlin Jordan <jordan.cait17@gmail.com>
2023-12-18 20:31:53 -0800

    added rough draft of helpers

--- lib/models/city.py
+++ lib/models/city.py
@@ -37,7 +37,122 @@ class City:
                 "State must be a non-empty string"
             )
     
+    @classmethod
+    def create_table(cls):
+        """ Create a new table to persist the attributes of City instances """
+        sql = """
+            CREATE TABLE IF NOT EXISTS cities (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            state TEXT)
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        """ Drop the table that persists City instances """
+        sql = """
+            DROP TABLE IF EXISTS cities;
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):
+        """ Insert a new row with the name and location values of the current City instance.
+        Update object id attribute using the primary key value of new row.
+        Save the object in local dictionary using table row's PK as dictionary key"""
+        sql = """
+            INSERT INTO cities (name, location)
+            VALUES (?, ?)
+        """
+
+        CURSOR.execute(sql, (self.name, self.state))
+        CONN.commit()
+
+        self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
+
+    @classmethod
+    def create(cls, name, state):
+        """ Initialize a new City instance and save the object to the database """
+        city = cls(name, state)
+        city.save()
+        return city
     
+    def delete(self):
+        """Delete the table row corresponding to the current City instance,
+        delete the dictionary entry, and reassign id attribute"""
+
+        sql = """
+            DELETE FROM cities
+            WHERE id = ?
+        """
+
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
+        del type(self).all[self.id]
+        self.id = None
+
+
+    @classmethod
+    def instance_from_db(cls, row):
+        """Return a City object having the attribute values from the table row."""
+        city = cls.all.get(row[0])
+        if city:
+            city.name = row[1]
+            city.state = row[2]
+        else:
+            city = cls(row[1], row[2])
+            city.id = row[0]
+            cls.all[city.id] = city
+        return city
+    
+    @classmethod
+    def get_all(cls):
+        """Return a list containing a City object per row in the table"""
+        sql = """
+            SELECT *
+            FROM cities
+        """
+
+        rows = CURSOR.execute(sql).fetchall()
+        return [cls.instance_from_db(row) for row in rows]
+    
+    @classmethod
+    def find_by_id(cls, id):
+        """Return a City object corresponding to the table row matching the specified primary key"""
+        sql = """
+            SELECT *
+            FROM cities
+            WHERE id = ?
+        """
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        return cls.instance_from_db(row) if row else None
+    
+    @classmethod
+    def find_by_name(cls, name):
+        """Return a City object corresponding to first table row matching specified name"""
+        sql = """
+            SELECT *
+            FROM cities
+            WHERE name is ?
+        """
+        row = CURSOR.execute(sql, (name,)).fetchone()
+        return cls.instance_from_db(row) if row else None
+    
+    def restaurants(self):
+        """Return list of restaurants in current city"""
+        from models.restaurant import Restaurant
+        sql = """
+            SELECT * FROM restaurants
+            WHERE city_id = ?
+        """
+        CURSOR.execute(sql, (self.id,),)
+        rows = CURSOR.fetchall()
+        return [
+            Restaurant.instance_from_db(row) for row in rows
+        ]
 
     # @classmethods
     # create_table to persist attributes of City
--- lib/models/restaurant.py
+++ lib/models/restaurant.py
@@ -12,7 +12,7 @@ class Restaurant:
         self.cuisine = cuisine
 
     def __repr__(self):
-        return f'<City {self.id}: {self.name}, located in {self.city}>'
+        return f'<City {self.id}: {self.name}, located in {self.city_id}>'
     
     @property
     def name(self):
@@ -25,6 +25,128 @@ class Restaurant:
             raise ValueError(
                 "Name must be a non-empty string"
             )
+        
+    @property
+    def cuisine(self):
+        return self._cuisine
+    @cuisine.setter
+    def cuisine(self, cuisine):
+        if isinstance(cuisine, str) and len (cuisine):
+            self._cuisine = cuisine
+        else:
+            raise ValueError(
+                "Cuisine must be a non-empty string"
+            )
+        
+    @classmethod
+    def create_table(cls):
+        """ Create a new table to persist the attributes of Restaurant instances """
+        sql = """
+            CREATE TABLE IF NOT EXISTS restaurants (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            cuisine TEXT,
+            city_id INTEGER,
+            FOREIGN KEY (city_id) REFERENCES cities(id))
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        """Drop the table that persists Restaurant instances"""
+        sql = """
+            DROP TABLE IF EXISTS restaurants;
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):
+        """ Insert a new row with the name, cuisine, and city id values 
+        of the current Restaurant object. Update object id attribute using the 
+        PK value of new row. Save the obj in local dictionary using table
+        row's PK as dictionary key"""
+        sql = """
+            INSERT INTO restaurants (name, cuisine, city_id)
+            VALUES (?, ?, ?)
+            """
+        CURSOR.execute(sql, (self.name, self.cuisine, self.city_id))
+        CONN.commit()
+        self.id = CURSOR.lastrowid
+        type(self).all[self.id] = self
+
+    def delete(self):
+        """Delete the table row corresponding to the current Restaurant instance,
+        delete the dictionary entry, and reassign id attribute"""
+
+        sql = """
+            DELETE FROM restaurants
+            WHERE id = ?
+        """
+
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
+        del type(self).all[self.id]     
+
+
+    @classmethod
+    def create(cls, name, cuisine, city_id):
+        """ Initialize a new Restaurant instance and save the object to the database """
+        restaurant = cls(name, cuisine, city_id)
+        restaurant.save()
+        return restaurant
+
+    @classmethod
+    def instance_from_db(cls, row):
+        """Return an Restaurant object having the attribute values from the table row."""
+        restaurant = cls.all.get(row[0])
+        if restaurant:
+            restaurant.name = row[1]
+            restaurant.cuisine = row[2]
+            restaurant.city_id = row[3]
+        else:
+            # not in dictionary, create new instance and add to dictionary
+            restaurant = cls(row[1], row[2], row[3])
+            restaurant.id = row[0]
+            cls.all[restaurant.id] = restaurant
+        return restaurant   
+    
+    @classmethod
+    def get_all(cls):
+        """Return a list containing one Restaurant object per table row"""
+        sql = """
+            SELECT *
+            FROM restaurants
+        """
+
+        rows = CURSOR.execute(sql).fetchall()
+        return [cls.instance_from_db(row) for row in rows]
+    
+    @classmethod
+    def find_by_id(cls, id):
+        """Return Restaurant object corresponding to the table row matching the 
+        specified PK"""
+        sql = """
+            SELECT *
+            FROM restaurants
+            WHERE id = ?
+        """
+
+        row = CURSOR.execute(sql, (id,)).fetchone()
+        return cls.instance_from_db(row) if row else None    
+    
+    @classmethod
+    def find_by_name(cls, name):
+        """Return Restaurant object corresponding to first table row matching 
+        specified name"""
+        sql = """
+            SELECT *
+            FROM restaurants
+            WHERE name is ?
+        """
+
+        row = CURSOR.execute(sql, (name,)).fetchone()
+        return cls.instance_from_db(row) if row else None
 
     # @ properties
     # cuisine to validate cuisine is a non empty string
